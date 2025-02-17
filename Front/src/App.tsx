775ff5d2a795817c104a7b1f6d90a32ca04e7d27
miguel2433 <miguelitocap12@gmail.com>
2025-02-17 16:41:33 -0300

    dependencias

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-3xl font-bold underline text-red-500'>Hello World</h1>
+     <h1 className='font-bold underline text-red-500'>Hello World</h1>
     </>
   )
 }
