a8be24fc01cc857dc260aee1d735248fafdd8448
kamaleon19 <joaco.px@gmail.com>
2025-02-24 21:25:12 -0300

    module vehiculos finished

--- back/src/vehiculos/dto/create-vehiculo.dto.ts
+++ back/src/vehiculos/dto/create-vehiculo.dto.ts
@@ -1,1 +1,20 @@
-export class CreateVehiculoDto {}
+import { IsString, MaxLength, Min, MinLength } from "class-validator"
+
+export class CreateVehiculoDto {
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(50)
+    modelo: string
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(25)
+    marca: string
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(10)
+    patente: string
+
+}
--- back/src/vehiculos/vehiculos.module.ts
+++ back/src/vehiculos/vehiculos.module.ts
@@ -3,11 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Vehiculo } from './entities/vehiculo.entity';
 import { VehiculosService } from './vehiculos.service';
 import { VehiculosController } from './vehiculos.controller';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   imports: [
 
     TypeOrmModule.forFeature([Vehiculo]),
+
+
+    CommonModule
 
   ],
   controllers: [VehiculosController],
